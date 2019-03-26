694a716275c8b4cb2eb8c667bd2a2e46d87a5737
Oscar Leon <oscar@thebyte9.com>
2019-03-26 16:05:07 +0000

    BZ2-142 @jira Added blaze-ui styles on stories. Library will remain without styles.

--- src/Button/stories/Button.stories.js
+++ src/Button/stories/Button.stories.js
@@ -4,7 +4,7 @@ import Button from '../index';
 
 storiesOf('Button', module)
   .add('with children', () => (
-    <Button><span>Button with children</span></Button>
+    <Button className="button"><span>Button with children</span></Button>
   ))
   .add('with text', () => (
     <Button className="button">This is a button</Button>
