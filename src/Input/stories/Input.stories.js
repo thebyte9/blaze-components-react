05a4e4d77b825529f23acb891064951767e10774
Ismael Tanane <ismael@Ismaels-MacBook-Pro.local>
2019-03-28 12:57:54 +0000

    BZ2-157 @jira remove duplicated lines

--- src/Input/stories/Input.stories.js
+++ src/Input/stories/Input.stories.js
@@ -6,41 +6,9 @@ storiesOf('Inputs', module)
   .add('Text input', () => (
     <form>
       <div className="component-wrapper">
-
-        <div className="form-field form-field--input">
-          <Input
-            placeholder="Simple input"
-            onChange={() => {}}
-            />
-        </div>
-
-        <div className="form-field form-field--input">
-          <Input
-            label="Text input label + placeholder"
-            placeholder="Placeholder text"
-            onChange={() => {}}
-            />
-        </div>
-
-        <div className="form-field form-field--input form-field--small">
-          <Input
-            label="Text input small"
-            placeholder="Placeholder text"
-            onChange={() => {}}
-            />
-        </div>
-
         <div className="form-field form-field--input form-field--full-width">
           <Input
             label="Text input full width"
-            placeholder="Placeholder text"
-            onChange={() => {}}
-            />
-        </div>
-
-        <div className="form-field form-field--input">
-          <Input
-            label="Text input disabled"
             placeholder="Placeholder text"
             onChange={() => {}}
             />
