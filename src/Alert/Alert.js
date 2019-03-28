5fd5dfbf6d68b4093d22b2e249df09d183006907
Ismael Tanane <ismael@Ismaels-MacBook-Pro.local>
2019-03-28 16:55:29 +0000

    BZ2-164 @jira use own components

--- src/Alert/Alert.js
+++ src/Alert/Alert.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useState } from 'react';
 import PropTypes from 'prop-types';
+import Button from '../Button';
 
 const Alert = ({
   children,
@@ -18,9 +19,9 @@ const Alert = ({
       {icon && <i className="material-icons">{icon}</i>}
       {children}
       {close && (
-      <button onClick={() => setModalOff(true)} className="icon-button icon-button--close" type="button">
+      <Button onClick={() => setModalOff(true)} className="icon-button icon-button--close">
         <i className="material-icons">close</i>
-      </button>
+      </Button>
       )}
     </div>
   );
