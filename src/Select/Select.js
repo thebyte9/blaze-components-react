00e9b767e5513cb32545ab7365d8720ecd74b000
Ismael Tanane <ismael@Ismaels-MacBook-Pro.local>
2019-03-29 12:00:32 +0000

    BZ2-160 @jira component done

--- src/Select/Select.js
+++ src/Select/Select.js
@@ -6,28 +6,52 @@ const Select = ({
   required,
   onChange,
   options,
+  selected,
+  keys,
   ...attrs
 }) => {
-  const [, setSelectedOption] = useState(null);
+  const [selectedOption, setSelectedOption] = useState(selected);
 
-  const handleChange = (e = {
-    target: {}
-  }) => {
+  const handleChange = (e) => {
     setSelectedOption(e.target.value);
-    onChange({ e, value: e.target.value });
+    onChange({ e, selected: e.target.value });
   };
 
   const isRequired = required ? 'required' : '';
+  
+  const renderOptions = () => {
+    const [first] = options;
+
+    if (typeof first === 'string') {
+      return options.map(option => (
+        <option key={option} value={option}>{option}</option>
+      ));
+    }
+
+    if (first instanceof Array) {
+      return options.map(([value, text]) => (
+        <option key={value} value={value}>{text}</option>
+      ));
+    }
+
+    return options.map((option) => {
+      const [value, text] = keys;
+      return (
+        <option key={option[value]} value={option[value]}>{option[text]}</option>
+      );
+    });
+  };
+
   return (
     <Fragment>
       {label && <label htmlFor={attrs.id} className={isRequired}>{label}</label>}
-      <select onChange={handleChange} defaultValue={null} disabled={!options.length} {...attrs}>
+      <select
+        onChange={handleChange}
+        defaultValue={selectedOption}
+        disabled={!options.length}
+        {...attrs}>
         <option value={null}>Please Choose...</option>
-        {options.map(option => (
-          <option key={option} value={option}>
-            {option}
-          </option>
-        ))}
+        {renderOptions()}
       </select>
     </Fragment>
   );
@@ -37,6 +61,8 @@ Select.propTypes = {
   label: PropTypes.string,
   required: PropTypes.bool,
   options: PropTypes.array,
+  selected: PropTypes.any,
+  keys: PropTypes.array,
   onChange: PropTypes.func,
 };
   
@@ -44,7 +70,10 @@ Select.defaultProps = {
   label: '',
   options: [],
   required: false,
+  selected: null,
+  keys: [],
   onChange: () => {}
 };
 
 export default Select;
+
--- src/Select/stories/Select.stories.js
+++ src/Select/stories/Select.stories.js
@@ -3,6 +3,17 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Select from '../index';
 
+const arrayOfObjects = [
+  {
+    id: 1,
+    username: 'Oscar'
+  },
+  {
+    id: 2,
+    username: 'Ismael'
+  }
+];
+
 storiesOf('Select Input', module)
   .add('Basic select', () => (
     <form>
@@ -10,7 +21,30 @@ storiesOf('Select Input', module)
         <div className="form-field form-field--select">
           <Select
             label="Select label"
+            selected={null}
             options={['lorem', 'ipsum', 'dolor']}
+            onChange={() => {}}
+            required
+            />
+          <br />
+          <Select
+            label="Select label"
+            selected="08001"
+            options={[['08001', 'Barcelona'], ['17006', 'Madrid']]}
+            onChange={() => {}}
+            />
+          <br />
+          <Select
+            label="Select label"
+            selected="1"
+            options={arrayOfObjects}
+            keys={['id', 'username']}
+            onChange={() => {}}
+            />
+
+          <br />
+          <Select
+            label="Select label"
             onChange={() => {}}
             />
         </div>
