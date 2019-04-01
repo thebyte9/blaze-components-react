5bb7b520917b9127e58b2423074f1afc9d571067
Ismael Haytam <anonismaa@gmail.com>
2019-04-01 11:59:03 +0100

    BZ2-161 @jira textarea  done (#5)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import Button from './Button';
+import Textarea from './Textarea';
 import Input from './Input';
 import Alert from './Alert';
 
 export default {
   Button,
   Input,
+  Textarea,
   Alert
 };
