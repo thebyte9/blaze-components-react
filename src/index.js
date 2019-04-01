dcfc70a1e0aace130fc620ee93582c3e8845e726
Ismael Haytam <anonismaa@gmail.com>
2019-04-01 12:07:20 +0100

    Bz2 160/select component (#4)
    * BZ2-160 @jira add select component
    
    * BZ2-160 @jira add select component
    
    * BZ2-160 @jira component done
    
    * BZ2-160 @jira component done
    
    * BZ2-160 @jira refactor

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import Button from './Button';
+import Select from './Select';
 import Textarea from './Textarea';
 import Input from './Input';
 import Alert from './Alert';
@@ -6,6 +7,7 @@ import Alert from './Alert';
 export default {
   Button,
   Input,
+  Select,
   Textarea,
   Alert
 };
