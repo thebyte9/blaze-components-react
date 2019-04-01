8b77c77be1d752d6b21af42345513082f3576be6
Ismael Haytam <anonismaa@gmail.com>
2019-04-01 10:19:41 +0100

    Bz2 157/input component (#2)
    * BZ2-157 @jira add input component
    
    * BZ2-157 @jira improve tests
    
    * BZ2-157 @jira fix htmlFor
    
    * BZ2-157 @jira fix tests
    
    * BZ2-157 @jira remove duplicated lines
    
    * BZ2-157 @jira fix spelling mistake
    
    * BZ2-157 @jira improve required input
    
    * BZ2-157 @jira improve required input
    
    * BZ2-157 @jira improve required input
    
    * BZ2-157 @jira simantic names
    
    * BZ2-157 @jira fix test

---
+++ src/Input/__tests__/Input.test.js
@@ -0,0 +1,48 @@
+
+import React from 'react';
+import expect from 'expect';
+import { shallow, render } from 'enzyme';
+import Input from '../Input';
+
+const testProps = {
+  placeholder: 'Placeholder text',
+  onChange: () => {}
+};
+
+describe('Input component', () => {
+  test('should be defined and renders correctly (snapshot)', () => {
+    const wrapper = shallow(<Input {...testProps} />);
+    expect(wrapper).toBeDefined();
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  test('visibility should be off', () => {
+    const wrapper = render(<Input type="password" required {...testProps} />);
+
+    expect(wrapper.text()).toContain('Show');
+    expect(wrapper.text()).toContain('visibility_off');
+  });
+
+  test('should renders input with label', () => {
+    const wrapper = render(<Input label="Text input label" {...testProps} />);
+    expect(wrapper.find('label')).toBeDefined;
+  });
+
+  test('should show or hide password on toggle', () => {
+    const wrapper = shallow(<Input type="password" {...testProps} />);
+    const icon = wrapper.find('.material-icons');
+
+    wrapper.find('.show-hide-password').simulate('click');
+    expect(icon.text()).toContain('visibility');
+
+    wrapper.find('.show-hide-password').simulate('click');
+    expect(icon.text()).toContain('visibility_off');
+  });
+
+  test('should change input', () => {
+    const wrapper = shallow(<Input {...testProps} />);
+
+    wrapper.find('input').simulate('change', { target: { value: 'byte9' } });
+    expect(wrapper.find('input').prop('value')).toBe('byte9');
+  });
+});
