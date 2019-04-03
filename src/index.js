c23bf30eedd523d31061dfb9b9d77ad06cb2a6b7
Ismael Tanane <ismael@Ismaels-MacBook-Pro.local>
2019-04-03 13:13:15 +0100

    BZ2-168 @jira Progress component done

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import Select from './Select';
 import Textarea from './Textarea';
 import Input from './Input';
 import Alert from './Alert';
+import Progress from './Progress';
 
 export default {
   Button,
   Input,
   Select,
   Textarea,
-  Alert
+  Alert,
+  Progress
 };
