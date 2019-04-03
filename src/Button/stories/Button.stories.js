3282da3efa3df1c0b9c3a0576df44ca0f70626d0
SC <simon@thebyte9.com>
2019-04-03 13:28:25 +0100

    Add initial Button stories back in

--- src/Button/stories/Button.stories.js
+++ src/Button/stories/Button.stories.js
@@ -25,6 +25,37 @@ const showCaseButtonStyles = {
 };
 
 storiesOf('Button', module)
+  .add('Simple', () => (
+    <div className="component-wrapper">
+      <h1>Simple button with click event</h1>
+      <Button
+        className="button"
+        onClick={() => {}}>
+        Click
+      </Button>
+    </div>
+  ))
+  .add('Submit button', () => (
+    <form onSubmit={() => {}} className="component-wrapper">
+      <h1>Button inside form</h1>
+      <Button
+        className="button"
+        isSubmit>
+        <span>Submit</span>
+      </Button>
+    </form>
+  ))
+  .add('Disabled', () => (
+    <div className="component-wrapper">
+      <h1>Disabled button</h1>
+      <Button
+        className="button button--disabled"
+        onClick={() => {}}
+        disabled>
+        Disabled
+      </Button>
+    </div>
+  ))
   .add('Styling', () => (
     <div className="component-wrapper">
       <p>
