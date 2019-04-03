71d1ee4813bb8b16564c440b54c72291118ac96c
Ismael Tanane <ismael@Ismaels-MacBook-Pro.local>
2019-04-03 14:28:45 +0100

    BZ2-169 @jira Tooltip component done

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import Select from './Select';
 import Textarea from './Textarea';
 import Input from './Input';
 import Alert from './Alert';
+import Tooltip from './Tooltip';
 
 export default {
   Button,
   Input,
   Select,
   Textarea,
-  Alert
+  Alert,
+  Tooltip
 };
