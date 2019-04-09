5fcc3550affbafcd1560c187c097c9a2b41a0249
Ismael Tanane <ismael@Ismaels-MacBook-Pro.local>
2019-04-09 12:26:27 +0200

    BZ2-163 @jira fix htmlFor

--- src/RadioButton/stories/RadioButton.stories.js
+++ src/RadioButton/stories/RadioButton.stories.js
@@ -3,9 +3,13 @@ import { storiesOf } from '@storybook/react';
 import RadioButton from '../index';
 
 const options = [
-  { label: 'Example', value: 1 },
-  { label: 'I accept', value: 'accepted' },
-  { label: 'Disabled', value: '', disabled: true }
+  { label: 'Example', value: 1, id: 'one' },
+  {
+    label: 'I accept', value: 'accepted', required: true, id: 'two'
+  },
+  {
+    label: 'Disabled', value: '', disabled: true, id: 'three'
+  }
 ];
 
 storiesOf('RadioButton', module)
