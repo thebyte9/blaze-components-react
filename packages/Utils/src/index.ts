2280ddc55dadaed5fb88cc3b10eebae903e9dfec
Ismael Tanane <ismael@Ismaels-MacBook-Pro.local>
2019-07-01 11:49:28 +0200

    BZ2-330 @jira refactor

--- packages/Utils/src/index.ts
+++ packages/Utils/src/index.ts
@@ -1,30 +1,14 @@
 import { makeDecorator } from 'react-decorate';
-
-const generateKey = (element: any) => {
-  const key = unescape(encodeURIComponent(JSON.stringify(element)));
-
-  try { 
-      return btoa(key);
-  } catch {
-      return Buffer.from(key).toString('base64');
-  }
-}
+import uniqueId from './uniqueId';
 
 const utils = (propName = 'utils') => {
   
   return {
     initialState() {
       
-      const  uniqueId = (element: any) =>  {
-        const keys = {};
-        const key = `uid-${generateKey(element)}`;
-        keys[key] = key;
-        return key;
+      return { 
+        uniqueId 
       };
-
-      const printLog = (text: string) => text;
-
-      return { uniqueId, printLog };
 
     },
 
---
+++ packages/Utils/src/uniqueId.ts
@@ -0,0 +1,18 @@
+const generateKey = (element: any) => {
+    const key = unescape(encodeURIComponent(JSON.stringify(element)));
+
+    try { 
+        return btoa(key);
+    } catch {
+        return Buffer.from(key).toString('base64');
+    }
+}
+
+const  uniqueId = (element: any) =>  {
+    const keys = {};
+    const key = `uid-${generateKey(element)}`;
+    keys[key] = key;
+    return key;
+};
+
+export default uniqueId;
