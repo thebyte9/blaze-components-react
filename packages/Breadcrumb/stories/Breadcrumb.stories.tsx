a8cb8cd16e66e5e5f71fd4d30227a3022e0a0c62
Ismael Tanane <ismael@Ismaels-MacBook-Pro.local>
2019-07-02 20:14:26 +0200

    BZ2-Fix-Build @jira fix error - max memory usage

--- packages/Breadcrumb/stories/Breadcrumb.stories.tsx
+++ packages/Breadcrumb/stories/Breadcrumb.stories.tsx
@@ -1,5 +1,5 @@
-import { storiesOf } from "@storybook/react";
 import React from "react";
+import { storiesOf } from "@storybook/react";
 import BreadcrumbsReadme from "../README.md";
 import Breadcrumb from "../src";
 storiesOf("Breadcrumbs", module)
