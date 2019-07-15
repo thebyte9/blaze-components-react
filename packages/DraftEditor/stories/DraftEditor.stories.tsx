c9fd6716ea8a0a7437258236ef21fca5e98035f2
Ismael Tanane <ismael@Ismaels-MacBook-Pro.local>
2019-07-15 11:15:46 +0200

    BZ2-367 @jira remove styles

--- packages/DraftEditor/stories/DraftEditor.stories.tsx
+++ packages/DraftEditor/stories/DraftEditor.stories.tsx
@@ -12,15 +12,8 @@ storiesOf("DraftEditor", module)
     <div className="component-wrapper">
       <section className="introductionSection">
         <h1>DraftEditor</h1>
-        <p>
-          Provide contextual feedback messages for typical user actions with the
-          handful of available and flexible alert messages. Alerts may have a
-          type, be dismissable, include a close button, and contain any sort of
-          children components.
-        </p>
       </section>
 
-      <h4>Simple</h4>
       <DraftEditor />
     </div>
   ));
