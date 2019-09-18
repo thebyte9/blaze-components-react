14dcee857642b43648f25a16259d97497a6c431c
Ismael Tanane <ismael@Ismaels-MacBook-Pro.local>
2019-09-18 17:28:26 +0200

    fix modal classnames

--- packages/Modal/src/ModalHeader/ModalHeader.tsx
+++ packages/Modal/src/ModalHeader/ModalHeader.tsx
@@ -1,22 +1,17 @@
 import React from "react";
 
 interface IModalHeaderProps {
-  headerClassNames: string;
   title?: string;
   closeModal: () => void;
 }
 
-const ModalHeader = ({
-  headerClassNames,
-  title,
-  closeModal
-}: IModalHeaderProps): JSX.Element => (
-  <div className={headerClassNames}>
+const ModalHeader = ({ title, closeModal }: IModalHeaderProps): JSX.Element => (
+  <>
     <div className="modal__title">{title}</div>
     <div className="modal__close" role="button" onClick={closeModal}>
       <i className="material-icons">close</i>
     </div>
-  </div>
+  </>
 );
 
 export default ModalHeader;
