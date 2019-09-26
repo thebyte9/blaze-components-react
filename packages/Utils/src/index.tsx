c0062204064d14e08e25502f5f5f83511b57bf0f
Ismael Tanane <ismael@Ismaels-MacBook-Pro.local>
2019-09-26 21:22:49 +0200

    BZ2-442 @jira use proxy to handle dynamic imports - refactor

--- packages/Utils/src/index.tsx
+++ packages/Utils/src/index.tsx
@@ -2,20 +2,22 @@ import React, { FunctionComponent } from "react";
 
 export default (ChildComponent: any) => {
   const WithUtils: FunctionComponent = (props: object): JSX.Element => {
-    const handler = {
-      get({}, utilName: string) {
-        const allowedLibraries: string[] = [
-          "classNames",
-          "ErrorMessage",
-          "uniqueId",
-          "useDebounce"
-        ];
-        if (allowedLibraries.includes(utilName)) {
-          return require(`./${utilName}`).default;
+    const utils = new Proxy(
+      {},
+      {
+        get({}, libName: string) {
+          const allowedLibraries: string[] = [
+            "classNames",
+            "ErrorMessage",
+            "uniqueId",
+            "useDebounce"
+          ];
+          if (allowedLibraries.includes(libName)) {
+            return require(`./${libName}`).default;
+          }
         }
       }
-    };
-    const utils = new Proxy({}, handler);
+    );
     return <ChildComponent utils={utils} {...props} />;
   };
   return WithUtils;
