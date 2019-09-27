3685f14da4f6532d3feba90709c318503166d13c
Ismael Tanane <ismael@Ismaels-MacBook-Pro.local>
2019-09-27 09:01:28 +0200

    remove build folder

--- .typescript/lib/Accordion/src/AccordionContent/AccordionContentDetails/AccordionContentDetails.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="react" />
-interface IAccordionContentDetailsProps {
-    children: JSX.Element | JSX.Element[];
-}
-declare const AccordionContentDetails: ({ children }: IAccordionContentDetailsProps) => JSX.Element;
-export default AccordionContentDetails;
