6144ce556910ae0b47da81b6a73d952f6d6bcd00
Almudena Iturgoyen <almudena@thebyte9.com>
2019-10-02 16:47:33 +0200

    BZ2-Badge colours - Remove pill and round styles for badges

--- packages/Badge/src/index.tsx
+++ packages/Badge/src/index.tsx
@@ -13,33 +13,27 @@ const Badge: FunctionComponent<IBadgeProps> = ({
   children,
   type,
   color,
-  pill,
   icon,
-  round,
   to,
   link,
   ...attrs
 }) => {
-  const isPill = pill ? "badge--pill" : "";
-  const isRound = round ? "badge--round" : "";
   const withIcon = icon ? "badge--icon-text" : "";
-  const classes = `badge ${color} ${isRound} ${isPill} ${withIcon} ${type}`;
+  const classes = `badge ${type} ${withIcon} ${color}`;
   return link ? (
     <a href={to} className={classes} {...attrs}>
       {children}
     </a>
   ) : (
-      <span className={classes} {...attrs}>
-        {children}
-      </span>
-    );
+    <span className={classes} {...attrs}>
+      {children}
+    </span>
+  );
 };
 Badge.defaultProps = {
   children: "No content",
   icon: false,
   link: false,
-  pill: false,
-  round: false,
   to: "#",
   color: "grey-dark",
   type: "badge--pagebuilder"
