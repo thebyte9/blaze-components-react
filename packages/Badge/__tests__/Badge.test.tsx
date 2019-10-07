0a6da29162750d2e754e0d6378f6927c746e15b4
David <ddmarin94@gmail.com>
2019-10-07 15:14:17 +0200

    fix badge files

--- packages/Badge/__tests__/Badge.test.tsx
+++ packages/Badge/__tests__/Badge.test.tsx
@@ -3,8 +3,8 @@ import expect from "expect";
 import React from "react";
 import Badge from "../src";
 
-describe("Badge component", () => {
-  test("should render badge type link", () => {
+describe("Badge component", () => {
+  test("should render children when badge type is link", () => {
     const wrapper = shallow(
       <Badge type="pagebuilder" link>
         <a href="#"></a>
@@ -14,19 +14,50 @@ describe("Badge component", () => {
     expect(link.length).toBe(1);
   });
 
-  test("should render badge type icon", () => {
+  test("should render children within span when badge type is not link", () => {
     const wrapper = shallow(
-      <Badge type="pagebuilder" color="blue" icon>
-        Dolor
-        <i className="fas fa image" />
+      <Badge type="primary">
+        <a href="#"></a>
       </Badge>
     );
     const span = wrapper.find("span");
-    expect(span.text()).toContain("Dolor");
+    expect(span.length).toBe(1);
   });
 
-  test("should render badge without type", () => {
-    const wrapper = shallow(<Badge>Test Badge</Badge>);
-    expect(wrapper.prop("className")).toContain("badge");
+  test("should render correct class when badge type is not link", () => {
+    const wrapper = shallow(
+      <Badge type="pagebuilder" icon color="deep-orange">
+        Social follow         
+        <i className="fas fa-share" />
+      </Badge>
+    );
+    const span = wrapper.find("span");
+    expect(span.prop("className")).toContain("pagebuilder");
+    expect(span.prop("className")).toContain("badge--icon-text");
+    expect(span.prop("className")).toContain("deep-orange");
+  });
+
+  test("should render badge type icon and badge type text", () => {
+    const wrapper = shallow(
+      <Badge type="pagebuilder" color="blue" icon>
+        BadgeText         
+        <i className="fas fa image" />
+      </Badge>
+    );
+    const badge = wrapper.find("span");
+    expect(badge.text()).toContain("BadgeText");
+    const icon = wrapper.find("i");
+    expect(icon.length).toBe(1);
+  });
+
+  test("should display correct class based on props", () => {
+    const wrapper = shallow(
+      <Badge icon>
+        BadgeText
+        <i className="fas fa-follow" />
+      </Badge>
+    );
+    const span = wrapper.find("span");
+    expect(span.prop("className")).toContain("badge--icon-text");
   });
 });
