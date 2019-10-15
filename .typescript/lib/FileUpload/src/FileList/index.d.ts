847de3422c0b27bc5d2b2ad7e9940fbc6a4a5d75
Almudena Iturgoyen <almudena@thebyte9.com>
2019-10-15 12:28:53 +0200

    BZ2-556/Extend image settings

--- .typescript/lib/FileUpload/src/FileList/index.d.ts
+++ .typescript/lib/FileUpload/src/FileList/index.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="react" />
-declare const FileList: ({ previewImages, handleCancel }: {
+declare const FileList: ({ previewImages, handleCancel, handleInputChange }: {
     previewImages: any;
     handleCancel: any;
+    handleInputChange: any;
 }) => JSX.Element;
 export default FileList;
