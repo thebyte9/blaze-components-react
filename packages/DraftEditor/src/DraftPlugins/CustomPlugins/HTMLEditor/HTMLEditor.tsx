d2fa10757f919120b6bb1c3ae5d2a92c4d82a980
Ismael Tanane <ismael@Ismaels-MacBook-Pro.local>
2019-10-30 10:33:45 +0100

    format code

--- packages/DraftEditor/src/DraftPlugins/CustomPlugins/HTMLEditor/HTMLEditor.tsx
+++ packages/DraftEditor/src/DraftPlugins/CustomPlugins/HTMLEditor/HTMLEditor.tsx
@@ -1,18 +1,13 @@
 import React from "react";
+import { IHTMLProps } from "../../../interfaces";
 import StyleButton from "../StyleButton";
 
-import { IHTMLProps } from "../../../interfaces";
-
-const HTMLEditor = ({ onToggle, isDraftEditor }: IHTMLProps): JSX.Element => {
-  return (
-    <>
-      <StyleButton
-        label={<i className="material-icons">code</i>}
-        onToggle={onToggle}
-        active={!isDraftEditor}
-      />
-    </>
-  );
-};
+const HTMLEditor = ({ onToggle, isDraftEditor }: IHTMLProps): JSX.Element => (
+  <StyleButton
+    label={<i className="material-icons">code</i>}
+    onToggle={onToggle}
+    active={!isDraftEditor}
+  />
+);
 
 export default HTMLEditor;
