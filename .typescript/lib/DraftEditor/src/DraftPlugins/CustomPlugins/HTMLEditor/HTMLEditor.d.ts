1bcb15b341b06cdf07127b3ea6506d5810254519
Ismael Tanane <ismael@Ismaels-MacBook-Pro.local>
2019-10-30 16:25:50 +0100

    build story-book

---
+++ .typescript/lib/DraftEditor/src/DraftPlugins/CustomPlugins/HTMLEditor/HTMLEditor.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="react" />
+import { IHTMLProps } from "../../../interfaces";
+declare const HTMLEditor: ({ onToggle, isDraftEditor }: IHTMLProps) => JSX.Element;
+export default HTMLEditor;
