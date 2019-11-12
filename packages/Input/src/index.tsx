f7a7c75aab69340d96ab452bf9ed10c61585ea61
Natali <natali@thebyte9.com>
2019-11-12 11:14:10 +0100

    BZ2-XXX input didnt update if new value received (#166)
    * BZ2-XXX input didnt update if new value received
    
    * fix for test

--- packages/Input/src/index.tsx
+++ packages/Input/src/index.tsx
@@ -52,6 +52,8 @@ const Input: FunctionComponent<IInputProps> = ({
     setError(error);
   }, [error]);
 
+  useEffect(() => setNewValue(value || ""), [value]);
+
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
     const {
       target: { value: targetValue }
