4acc8c87e6c725dd5e6d1d37f1b0d0b676b95f03
David <ddmarin94@gmail.com>
2019-12-13 14:46:47 +0100

    moved placements into constant file

--- packages/Toaster/src/Toast/ToastContainer/ToastContainer.tsx
+++ packages/Toaster/src/Toast/ToastContainer/ToastContainer.tsx
@@ -1,14 +1,6 @@
 import React from "react";
+import { placements } from "../constants";
 import { IToastContainerProps } from "../types/container/container";
-
-const placements = {
-  "bottom-center": { bottom: 0, left: "50%", transform: "translateX(-50%)" },
-  "bottom-left": { bottom: 0, left: 0 },
-  "bottom-right": { bottom: 0, right: 0 },
-  "top-center": { top: 0, left: "50%", transform: "translateX(-50%)" },
-  "top-left": { top: 0, left: 0 },
-  "top-right": { top: 0, right: 0 }
-};
 
 const ToastContainer = ({
   hasToasts,
---
+++ packages/Toaster/src/Toast/constants.tsx
@@ -0,0 +1,10 @@
+const placements = {
+  "bottom-center": { bottom: 0, left: "50%", transform: "translateX(-50%)" },
+  "bottom-left": { bottom: 0, left: 0 },
+  "bottom-right": { bottom: 0, right: 0 },
+  "top-center": { top: 0, left: "50%", transform: "translateX(-50%)" },
+  "top-left": { top: 0, left: 0 },
+  "top-right": { top: 0, right: 0 }
+};
+
+export { placements };
