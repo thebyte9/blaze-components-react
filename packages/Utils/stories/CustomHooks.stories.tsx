1bb4b19805e63e0063df4c6e5ec035941944c833
David <ddmarin94@gmail.com>
2020-01-11 23:37:01 +0100

    deleted comment and log

--- packages/Utils/stories/CustomHooks.stories.tsx
+++ packages/Utils/stories/CustomHooks.stories.tsx
@@ -1,4 +1,3 @@
-// import "@blaze-react/blaze-components-theme";
 import { storiesOf } from "@storybook/react";
 import faker from "faker";
 import React from "react";
@@ -7,7 +6,6 @@ import { useInView } from "../src/customHooks";
 storiesOf("Video Container", module).add("Lazy load image", (): any => {
   const Image = ({ src, alt }: any) => {
     const [isIntersecting, outerRef]: any = useInView({ once: true });
-    console.log("isIntersecting", isIntersecting);
     return (
       <div ref={outerRef} style={{ height: "100px", width: "100px" }}>
         {isIntersecting && <img src={src} alt={alt} />}
