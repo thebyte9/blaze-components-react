a1767ddbd2a5c87e86992c1d709e59bc032c3ee3
David <ddmarin94@gmail.com>
2020-01-15 19:27:38 +0100

    refactor

--- packages/Dnd/src/utils/listWithChildren.ts
+++ packages/Dnd/src/utils/listWithChildren.ts
@@ -3,7 +3,7 @@ import uuid from "uuid";
 const listWithChildren = (list: any[], childrenProp: any): any =>
   list.map(item => ({
     ...item,
-    id: item.id ? item.id : uuid(),
+    id: item.id || uuid(),
     [childrenProp]: item[childrenProp]
       ? listWithChildren(item[childrenProp], childrenProp)
       : null
