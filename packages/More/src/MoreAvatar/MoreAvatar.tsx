07dbc074df539a244327d79e8f53736030771021
Almu Iturgoyen <almudena.iturgoyen@gmail.com>
2020-04-28 12:27:03 +0200

    BZ2-XX Commit after build

--- packages/More/src/MoreAvatar/MoreAvatar.tsx
+++ packages/More/src/MoreAvatar/MoreAvatar.tsx
@@ -27,7 +27,6 @@ const MoreAvatar: React.SFC<IMoreAvatarProps> = ({
     "icon-button icon-button--round": isMoreMenu,
     [className as string]: Boolean(className),
   });
-
   return (
     <Button onClick={handleToggle} className={buttonClassName} {...props}>
       {isHeader ? (
