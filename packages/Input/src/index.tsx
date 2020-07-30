f98468095595e4855dc85821145752817d9f842b
Daniel Villegas <43497439+vior1992@users.noreply.github.com>
2020-07-30 15:09:27 +0300

    BZ2-1697/Handle-number-0 @jira update useEffect (#367)

--- packages/Input/src/index.tsx
+++ packages/Input/src/index.tsx
@@ -52,7 +52,10 @@ const Input: FunctionComponent<IInputProps> = ({
     setError(error);
   }, [error]);
 
-  useEffect(() => setNewValue(value || ""), [value]);
+  useEffect(() => {
+    const newValue = value === undefined || value === null ? "" : value
+    setNewValue(newValue)
+  }, [value]);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
     const {
