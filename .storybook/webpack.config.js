e3acd897a85801be5449578d9d71f5e7e7a8fe25
Ismael Tanane <ismael@thebyte9.com>
2020-11-24 15:35:05 +0100

    update code editor

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -24,8 +24,11 @@ module.exports = ({config}) => {
   }
   );
   config.resolve.extensions.push('.ts', '.tsx');
+  config.resolve.alias = {
+    "monaco-editor": "monaco-editor/esm/vs/editor/editor.api.js"
+  };
   config.plugins.push(new MonacoWebpackPlugin({
-    languages: ['javascript', 'typescript', 'json']
+    languages: ['json']
   }))
   return config;
 };
