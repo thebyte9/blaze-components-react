1982fda273202f1d8be0b33a8688c7c058e1e074
Ryan Flores <ryan@thebyte9.com>
2021-04-13 12:06:51 +0300

    chore: update brandUrl

--- .storybook/blaze-theme.js
+++ .storybook/blaze-theme.js
@@ -3,6 +3,7 @@ import { create } from '@storybook/theming';
 export default create({
   base: 'light',
   brandTitle: 'Blaze',
-  brandUrl: 'https://blaze.com',
-  brandImage: 'https://media.licdn.com/dms/image/C4E0BAQGV39e-8T5Dvg/company-logo_200_200/0?e=2159024400&v=beta&t=hq2eQl1cfgILFgMc5TJUGKf3hF416BqbJf33Wx1dQEw',
+  brandUrl: 'https://blazecms.app',
+  brandImage:
+    'https://media.licdn.com/dms/image/C4E0BAQGV39e-8T5Dvg/company-logo_200_200/0?e=2159024400&v=beta&t=hq2eQl1cfgILFgMc5TJUGKf3hF416BqbJf33Wx1dQEw',
 });
