b2f409c153cf70d163fc0ef7ab6f464a94ef4ce1
Ismael Haytam <ismaelhaytamtanane@gmail.com>
2023-10-03 17:31:09 +0100

    feat: update tabs to handle conditional items (#676)
    * feat: update tabs to handle conditional items
    
    * fix: update tab stories
    
    * fix: remove extra spaces

--- packages/Tab/stories/Tab.stories.tsx
+++ packages/Tab/stories/Tab.stories.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { storiesOf } from "@storybook/react";
+import { Button } from '@blaze-react/button';
 import { Tab, TabItem } from "../src";
 import TabReadme from "../README.md";
 storiesOf("Tab", module)
@@ -11,6 +12,8 @@ storiesOf("Tab", module)
   .add("Introduction", () => {
     const Wrapper = () => {
       const [content, setContent] = useState("Loading...");
+      const [showOtherContent, setShowOtherContent] = useState(false);
+
       const loadContent = () =>
         setTimeout(
           () => setContent("Dynamic content successfully loaded"),
@@ -23,11 +26,16 @@ storiesOf("Tab", module)
             Tabs organize a static or dynamic content across different screens.
           </p>
           <Tab selected={0}>
-            <TabItem title="Basic">Basic content here</TabItem>
+            <TabItem title="Basic">
+              <p>Basic content here</p>
+              <p>
+                <Button onClick={(): void => setShowOtherContent(!showOtherContent)} label='Toggle Other Tab' />
+              </p>
+            </TabItem>
             <TabItem title="Advanced" action={loadContent}>
               {content}
             </TabItem>
-            <TabItem title="Other">Other content here</TabItem>
+            {showOtherContent && <TabItem title="Other">Other content here</TabItem>}
           </Tab>
         </div>
       );
