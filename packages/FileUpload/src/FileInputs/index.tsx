50f73691351056353749d6c5cc29664d02882c65
Khaoula Tanane <khaoulatanane@Khaoulas-MacBook-Pro-2.local>
2023-10-04 18:48:12 +0200

    chore: add reacts icons and copy icon

--- packages/FileUpload/src/FileInputs/index.tsx
+++ packages/FileUpload/src/FileInputs/index.tsx
@@ -3,6 +3,8 @@ import Select from '@blaze-react/select';
 import React from 'react';
 import { IMAGE, INPUT_TYPES, NAME } from '../constants';
 import { getInputLabel, sanitizedFilename } from '../utils';
+import { MdOutlineCopyAll } from 'react-icons/md';
+
 const { ALT_TEXT, CAPTION, HREF_URL, TITLE, CREDITS } = INPUT_TYPES;
 const FileInputs = ({
   data,
@@ -25,51 +27,54 @@ const FileInputs = ({
 }) => (
   <>
     <Input
-      label={getInputLabel(TITLE, file.type)}
+      label={<span className='input-label'>{getInputLabel(TITLE, file.type)}<MdOutlineCopyAll /> </span>}
       onChange={handleInputChange}
       value={name}
       id={`${index}-title-${sanitizedFilename(file)}`}
       name={NAME}
     />
+
     <Input
-      label={getInputLabel(CAPTION, file.type)}
+      label={<span className='input-label'>{getInputLabel(CAPTION, file.type)}<MdOutlineCopyAll /> </span>}
       onChange={handleInputChange}
       value={data.caption}
       id={`${index}-caption-${sanitizedFilename(file)}`}
       name={`${CAPTION}-${index}-${sanitizedFilename(file)}`}
     />
-    <span role='button' onClick={() => copyToOthers(CAPTION, index)}>Copy</span>
+
     <Input
-      label={getInputLabel(CREDITS, file.type)}
+      label={<span className='input-label'>{getInputLabel(CREDITS, file.type)}<MdOutlineCopyAll /> </span>}
       onChange={handleInputChange}
       value={data.credits}
       id={`${index}-credits-${sanitizedFilename(file)}`}
       name={`${CREDITS}-${index}-${sanitizedFilename(file)}`}
     />
-    {file.type === IMAGE && (
-      <>
-        <Input
-          label="Alternative text"
-          onChange={handleInputChange}
-          value={data.altText}
-          id={`${index}-altText-${sanitizedFilename(file)}`}
-          name={`${ALT_TEXT}-${index}-${sanitizedFilename(file)}`}
-        />
-        <Input
-          label="Url"
-          onChange={handleInputChange}
-          value={data.urlRedirect}
-          id={`${index}-hrefUrl-${sanitizedFilename(file)}`}
-          name={`${HREF_URL}-${index}-${sanitizedFilename(file)}`}
-        />
-        <Select
-          label="Store type"
-          data-testid="store-type"
-          options={selectOptions}
-          onChange={(event: any) => handleSelectChange(event, index)}
-        />
-      </>
-    )}
+    {
+      file.type === IMAGE && (
+        <>
+          <Input
+            label="Alternative text"
+            onChange={handleInputChange}
+            value={data.altText}
+            id={`${index}-altText-${sanitizedFilename(file)}`}
+            name={`${ALT_TEXT}-${index}-${sanitizedFilename(file)}`}
+          />
+          <Input
+            label="Url"
+            onChange={handleInputChange}
+            value={data.urlRedirect}
+            id={`${index}-hrefUrl-${sanitizedFilename(file)}`}
+            name={`${HREF_URL}-${index}-${sanitizedFilename(file)}`}
+          />
+          <Select
+            label="Store type"
+            data-testid="store-type"
+            options={selectOptions}
+            onChange={(event: any) => handleSelectChange(event, index)}
+          />
+        </>
+      )
+    }
   </>
 );
 export default FileInputs;
