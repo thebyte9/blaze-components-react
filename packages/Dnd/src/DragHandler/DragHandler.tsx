879c992af088d8bac3dbc1497ced8b6b7feb504a
Khaoula Tanane <khaoulatanane@Khaoulas-MacBook-Pro-2.local>
2023-10-18 19:31:24 +0200

    feat: add flag to control draggable item

--- packages/Dnd/src/DragHandler/DragHandler.tsx
+++ packages/Dnd/src/DragHandler/DragHandler.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 interface IDragHandlerProps {
   onDragStart: (...args: any[]) => any;
+  isDraggable: boolean
 }
-const DragHandler: React.SFC<IDragHandlerProps> = ({ onDragStart }) => (
-  <div className="list__drag" onDragStart={onDragStart} draggable data-testid="drag-handler">
-    <i className="material-icons">drag_indicator</i>
+const DragHandler: React.SFC<IDragHandlerProps> = ({ onDragStart, isDraggable }) => (
+  <div className="list__drag" onDragStart={onDragStart} draggable={isDraggable} data-testid="drag-handler">
+    <i className="material-icons">{isDraggable ? 'drag_indicator' : 'drag_disabled'}</i>
   </div>
 );
 export default DragHandler;
