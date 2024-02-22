35f0d9cd8b0bfa42d779719fe102e7d6cbbce96b
arcoders <ismaelhaytamtanane@gmail.com>
2024-02-22 20:50:39 +0100

    fix: remove unused import

--- packages/Input/src/view/Text/Input.tsx
+++ packages/Input/src/view/Text/Input.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import camelCase from 'lodash.camelcase';
-
 import { DisplayErrorAs, InputState, TextInputProps } from '../../types';
 import { ErrorMessage } from './ErrorMessage';
 import { ErrorIcon } from './Icons/ErrorIcon';
