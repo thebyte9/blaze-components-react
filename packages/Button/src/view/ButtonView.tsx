fdd6ec2cbf14e632563e06516f020e19d85fafa3
Khaoula Tanane <khaoulatanane@Khaoulas-MacBook-Pro-2.local>
2024-07-26 13:02:23 +0200

    chore: updates button props and snapshots

--- packages/Button/src/view/ButtonView.tsx
+++ packages/Button/src/view/ButtonView.tsx
@@ -2,38 +2,30 @@ import React from 'react';
 import { buildClassNames } from '@blaze-react/utils';
 import { IButtonViewProps } from '../types';
 
-export const ButtonView = ({ children, classes = '', label, ...rest }: IButtonViewProps): JSX.Element => {
-  const { modifiers = [], disabled, type = 'button' } = rest;
-
+export const ButtonView = ({
+  children,
+  classes = '',
+  label,
+  modifiers = [],
+  disabled,
+  type = 'button',
+  ...rest
+}: IButtonViewProps): JSX.Element => {
   const DeprecatedButton = () => {
-    console.warn('Modifiers will be deprecated in the near future. You should use CSS classes classes instead.');
-
     const formattedModifiers: string = modifiers.map((modifier) => `button--${modifier}`).join(' ');
     const buttonClassNames: string = buildClassNames('button', {
       [formattedModifiers]: !!modifiers,
     });
 
     return (
-      <button
-        disabled={disabled}
-        className={buttonClassNames}
-        type={type}
-        aria-label={label}
-        {...rest}
-      >
+      <button disabled={disabled} className={buttonClassNames} type={type} aria-label={label} {...rest}>
         {children}
       </button>
     );
   };
 
   const BlazeButton = (
-    <button
-      type="button"
-      className={classes}
-      disabled={disabled}
-      aria-label={label}
-      {...rest}
-    >
+    <button type="button" className={classes} disabled={disabled} aria-label={label} {...rest}>
       {children}
     </button>
   );
