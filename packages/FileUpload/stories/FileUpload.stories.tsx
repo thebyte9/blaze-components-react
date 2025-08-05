aac904d45b30a4c986a97283fd16fd795f153a71
arcoders <ismaelhaytamtanane@gmail.com>
2025-08-05 12:28:48 +0200

    fix: fix preselect

--- packages/FileUpload/stories/FileUpload.stories.tsx
+++ packages/FileUpload/stories/FileUpload.stories.tsx
@@ -18,7 +18,7 @@ storiesOf('FileUpload', module)
 
       return (
 
-        <FileUpload onChange={onChange} storeKey="maps" selectOptions={[['default', 'Default'], ['maps', 'Maps store']]} />
+        <FileUpload onChange={onChange} selectOptions={[['default', 'Default'], ['maps', 'Maps store']]} />
       );
     };
     return (
