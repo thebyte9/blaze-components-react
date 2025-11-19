42df22a1cdaf3d59b78a2c4cc706cf2eb98d3bc9
Emmanuel Mclean <58271203+emcleandev@users.noreply.github.com>
2025-11-19 15:27:46 +0000

    chore: update

--- packages/Input/src/deprecated/Input.tsx
+++ packages/Input/src/deprecated/Input.tsx
@@ -61,7 +61,7 @@ const Input: FunctionComponent<IInputProps> = ({
       target: { value: targetValue },
     } = event;
     setNewValue(targetValue);
-    onChange && onChange({ event, value: targetValue });
+    onChange({ event, value: targetValue });
   };
 
   const handleToggleType = (inputType: string): void => {
@@ -113,6 +113,7 @@ Input.defaultProps = {
   modifier: '',
   required: false,
   type: 'text',
+  onChange: ()=>{},
   validationMessage: 'This field is required',
 };
 
