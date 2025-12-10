08509e956f6781a2dbb5fde8647e3c9bd221eb73
arcoders <ismaelhaytamtanane@gmail.com>
2025-12-10 10:09:57 +0100

    fix: remove incompatible tailwind dark mode addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -77,7 +77,6 @@ module.exports = {
           '@blaze-react/pagination': path.resolve(__dirname, '../packages/Pagination/src'),
           '@blaze-react/tooltip': path.resolve(__dirname, '../packages/Tooltip/src'),
           '@blaze-react/text-area': path.resolve(__dirname, '../packages/Textarea/src'),
-          '@blaze-react/select': path.resolve(__dirname, '../packages/Select/src'),
           
         },
       },
