201aa9a04f51dcb63aee51b9c8ac5c7dae610a96
arcoders <ismaelhaytamtanane@gmail.com>
2025-12-12 16:10:38 +0100

    fix: move local peerdependencies to be dependencies

--- .storybook/main.js
+++ .storybook/main.js
@@ -76,7 +76,6 @@ module.exports = {
           '@blaze-react/pagination': path.resolve(__dirname, '../packages/Pagination/src'),
           '@blaze-react/tooltip': path.resolve(__dirname, '../packages/Tooltip/src'),
           '@blaze-react/text-area': path.resolve(__dirname, '../packages/Textarea/src'),
-          
         },
       },
     };
